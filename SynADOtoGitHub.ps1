73b280e1ea24f30a5d1b08d1e8e4e2e45f3d7e40
vinodkumar <114020915+vinodkumar704@users.noreply.github.com>
2023-02-22 21:50:17 +0530

    Update SynADOtoGitHub.ps1

--- SynADOtoGitHub.ps1
+++ SynADOtoGitHub.ps1
@@ -51,12 +51,12 @@ git remote add --mirror=fetch secondary $destURL
 Write-Output '*****Git fetch origin****'
 git fetch $sourceURL
 Write-Output '*****Git push secondary****'
-#git remote set-url origin $destURLSetURL
-#git push secondary  --all -f
-#Write-Output '**Azure Devops repo synced with Github repo**'
-#Set-Location $stageDir
-#if((Test-Path -path $githubDir))
-#{
- #Remove-Item -Path $githubDir -Recurse -force
-#}
-#write-host "Job completed"
+git remote set-url origin $destURLSetURL
+git push secondary  --all -f
+Write-Output '**Azure Devops repo synced with Github repo**'
+Set-Location $stageDir
+if((Test-Path -path $githubDir))
+{
+ Remove-Item -Path $githubDir -Recurse -force
+}
+write-host "Job completed"
