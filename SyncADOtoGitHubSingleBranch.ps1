80034b8ca08bb7abaa002529d5bcc0a08767c053
vinodkumar <114020915+vinodkumar704@users.noreply.github.com>
2023-03-13 11:13:59 +0530

    Update SyncADOtoGitHubSingleBranch.ps1

--- SyncADOtoGitHubSingleBranch.ps1
+++ SyncADOtoGitHubSingleBranch.ps1
@@ -1,12 +1,43 @@
+param(
+     [Parameter()]
+     [string]$GitHubDestinationPAT,
+ 
+     [Parameter()]
+     [string]$ADOSourcePAT
+ )
+
+# Write your PowerShell commands here.
+Write-Host ' - - - - - - - - - - - - - - - - - - - - - - - - -'
+Write-Host ' reflect Azure Devops repo changes to GitHub repo'
+Write-Host ' - - - - - - - - - - - - - - - - - - - - - - - - - '
+$AzureRepoName = "nestjs-project"
+$ADOCloneURL = "dev.azure.com/BrainvireInfo/IT-Project/_git/nestjs-project"
+$GitHubCloneURL = "github.com/vinodkumar704/nestjs-project.git"
+$stageDir = pwd | Split-Path
+Write-Host "stage Dir is : $stageDir"
+$githubDir = $stageDir +"\"+"gitHub"
+Write-Host "github Dir : $githubDir"
+$destination = $githubDir+"\"+ $AzureRepoName+".git"
+Write-Host "destination: $destination"
+#Please make sure, you remove https from azure-repo-clone-url
+$sourceURL = "https://$($ADOSourcePAT)"+"@"+"$($ADOCloneURL)"
+write-host "source URL : $sourceURL"
+#Please make sure, you remove https from github-repo-clone-url
+write-host "tocken : $GitHubDestinationPAT"
+write-host "clone URL : $GitHubCloneURL"
+$destURL = "https://" + $($GitHubDestinationPAT) +"@"+"$($GitHubCloneURL)"
+write-host "dest URL : $destURL"
+
+
 # Set the source and destination repositories
-$sourceRepo = "https://dev.azure.com/yourorg/yourproject/_git/yourrepo"
-$destinationRepo = "https://github.com/yourorg/yourrepo.git"
+$sourceRepo = $sourceURL
+$destinationRepo = $destURL
 
 # Set the name of the branch you want to mirror
 $branchName = "main"
 
 # Set the path to a temporary directory where the clone will be created
-$tempDir = "C:\temp"
+$tempDir = $githubDir
 
 # Clone the source repository
 Write-Host "Cloning $sourceRepo to $tempDir"
@@ -30,3 +61,4 @@ git push $destinationRepo $branchName
 # Clean up the temporary directory
 Write-Host "Cleaning up"
 Remove-Item -Recurse -Force $tempDir
+
